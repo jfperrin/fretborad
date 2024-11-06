6b9a6fafa1746cf7bdbb9507330a2a814a51730d
jeff <jfperrin@artprice.com>
2024-11-06 16:03:29 +0100

    redux

--- src/app/guitar/page.js
+++ src/app/guitar/page.js
@@ -4,14 +4,38 @@ import Fretboard from "@/app/guitar/ui/Fretboard";
 import { Select, SelectItem } from "@nextui-org/react";
 import styles from "@/app/guitar/page.module.scss";
 import { useDispatch, useSelector } from "react-redux";
-import { getNotePositions, setSelectedNote } from "@/app/guitar/ui/slice";
+import {
+  getNotePositions,
+  getSelectedNote,
+  setSelectedNote,
+} from "@/app/guitar/ui/slice";
+import { useEffect } from "react";
 
 export default function Page() {
   const dispatch = useDispatch();
   const notePositions = useSelector(getNotePositions);
+  const selectedNote = useSelector(getSelectedNote);
+
+  function randomIntFromInterval(min, max) {
+    // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      dispatch(
+        setSelectedNote(
+          Object.keys(notePositions)[randomIntFromInterval(1, 7 - 1)],
+        ),
+      );
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [dispatch, notePositions]);
 
   return (
     <div className={styles.guitarContainer}>
+      <div className={styles.selectedNote}>{selectedNote}</div>
       <Select
         label="Note sur le manche"
         placeholder="Selectionnez une note"
